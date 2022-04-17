854510c1f2cb0eeacb5356fdfdda1b73c5bab6e3
Hoang Son <soonhoang14@gmail.com>
2022-04-17 11:18:19 +0700

    build base

---
+++ server/controllers/userController.js
@@ -0,0 +1,13 @@
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const Users = require("../models/userModel");
+
+const userCtrl = {
+    register: async (req, res) => {
+
+    },
+    login: async (req, res) => {
+
+    }
+}
+module.exports = userCtrl;
