b122a72ae71a2d43d908d424dd50869ad1718d5f
Tuyen Pham <tuyen197.it@gmail.com>
2022-04-20 15:05:22 +0700

    fix: add file  QueryMethod

---
+++ server/Utils/QueryMethod.js
@@ -0,0 +1,22 @@
+class QueryMethod {
+	query = {};
+	method = null;
+	constructor(query, method) {
+		this.query = query;
+		this.method = method;
+	}
+	pagination() {
+		const limit = this.query.limit || 0;
+		const page = this.query.page || 0;
+
+		const skip = (page - 1) * limit;
+        this.method = this.method.skip(skip).limit(limit)
+		return this;
+	}
+    sort(){
+        const type = this.query.sort || "";
+        this.method = this.method.sort({createdAt: type})
+        return this;
+    }
+}
+module.exports = QueryMethod;
