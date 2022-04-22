794784e97c875046051f15ba3be8d41066910287
Tuyen Pham <tuyen197.it@gmail.com>
2022-04-22 19:44:40 +0700

    hihi

--- server/models/PostModel.js
+++ server/models/PostModel.js
@@ -6,11 +6,7 @@ const postSchema = new mongoose.Schema(
 		title: { type: String , default: "đây là tiêu đề"},
 		content: { type: String , default: "đây là nội dung"},
 		tags: { type: Array, default: ["nodejs", "reactjs"]},
-<<<<<<< HEAD
 		slug: {type: String, unique: true, required: true}
-=======
-		slug: {type: String, unique: true, require: true},
->>>>>>> 32c490a2e50be894e09e424f471c9642338122a4
 	},
 	{
 		timestamps: true,
